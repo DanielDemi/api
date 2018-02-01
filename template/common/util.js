f480cd02f86a8a2910f09d1dc6487382e5539557
zhangxin14 <zhangxin14@hikvision.com.cn>
2018-02-01 17:42:06 +0800

    「chg」修改eslint的校验问题

--- template/common/util.js
+++ template/common/util.js
@@ -60,17 +60,17 @@ function createAPI (baseURL) {
   }
 }
 
-function convertRESTAPI(url, opts) {
-  if (!opts || !opts.path) return url;
+function convertRESTAPI (url, opts) {
+  if (!opts || !opts.path) return url
 
-  const pathKeys = Object.keys(opts.path);
+  const pathKeys = Object.keys(opts.path)
 
   pathKeys.forEach((key) => {
-    const r = new RegExp('(:' + key + '|{' + key + '})', 'g');
-    url = url.replace(r, opts.path[key]);
-  });
+    const r = new RegExp('(:' + key + '|{' + key + '})', 'g')
+    url = url.replace(r, opts.path[key])
+  })
 
-  return url;
+  return url
 }
 
 export {
