ec245f51662f8dffebee4e12c161bacc3bf0c1aa
zhangxin14 <zhangxin14@hikvision.com.cn>
2018-02-01 17:39:18 +0800

    「chg」增加restapi转化方法

--- template/common/util.js
+++ template/common/util.js
@@ -60,7 +60,21 @@ function createAPI (baseURL) {
   }
 }
 
+function convertRESTAPI(url, opts) {
+  if (!opts || !opts.path) return url;
+
+  const pathKeys = Object.keys(opts.path);
+
+  pathKeys.forEach((key) => {
+    const r = new RegExp('(:' + key + '|{' + key + '})', 'g');
+    url = url.replace(r, opts.path[key]);
+  });
+
+  return url;
+}
+
 export {
   createAPI,
+  convertRESTAPI,
   http
 }
